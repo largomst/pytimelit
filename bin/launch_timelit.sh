a2ecb111f6246722bc705087325adfe1f6e28e6c
largomst <largomst@users.noreply.github.com>
2018-12-31 22:30:04 +0800

    fix launch path error

--- bin/launch_timelit.sh
+++ bin/launch_timelit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-lipc-set-prop com.lab126.pillow disableEnablePillow disable&
-python ./pytimelit.py&
+lipc-set-prop com.lab126.pillow disableEnablePillow disable
+python /mnt/base-us/extensions/pytimelit/bin/timelit.py
